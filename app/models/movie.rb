536aa80fb3e58d5bc4db98cc9be6dcc105a53443
Raghu Betina <raghu@firstdraft.com>
2016-11-15 16:23:30 +0000

    Validate length of movies.duration

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,8 @@ class Movie < ApplicationRecord
 
   # Validations
 
+  validates :duration, :length => { :minimum => 0, :maximum => 2764800 }
+
   validates :year, :length => { :minimum => 1870, :maximum => 2050 }
 
 end
